0fbd876b166be09903588b1030044f74e183dbc9
David Soliz <david.soliz@bolivia.assuresoft.com>
2024-04-25 15:17:39 -0400

    Adding environments varibales

--- cypress.config.js
+++ cypress.config.js
@@ -1,22 +1,32 @@
 const { defineConfig } = require("cypress");
-
 module.exports = defineConfig({
-  viewportHeight : 1080,
-  viewportWidth : 1920, 
-  video: false,
-  env: {   
-    username: 'testdavichin1@gmail.com',
-    password: 'testdavichin1',
-    apiUrl: 'https://conduit-api.bondaracademy.com'
-    
-  },
 
+//import { defineConfig } from 'cypress'
+//export default defineConfig({
+
+  viewportHeight: 1080,
+  viewportWidth: 1920,
+  video: false,
+  env: {
+    username: '',
+    password: '',
+    apiUrl: 'https://conduit-api.bondaracademy.com'
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      const username = process.env.DB_USERNAME
+      const password = process.env.PASSWORD
+
+        // if(!password){
+        //   throw new Error(`Missing PASSWORD environment variable`)
+        // }
+      config.env = {username, password}
+      return config
+
     },
     baseUrl: "https://conduit.bondaracademy.com/",
-    specPattern:'cypress/e2e/**/*.spec.js'
+    specPattern: 'cypress/e2e/**/*.spec.{js,jsx,ts,tsx}'
     //excludeSpecPattern: '**/examples/*' 
   },
 });
